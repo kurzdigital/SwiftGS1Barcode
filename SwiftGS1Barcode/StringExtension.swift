580f61ff1b952c6246961cb054d9dce3606200b4
Alexander Manzer <alexander.manzer@kurzdigital.com>
2023-12-13 10:07:17 +0100

    Fix some possible crashes in StringExtension because of missing range check

--- SwiftGS1Barcode/StringExtension.swift
+++ SwiftGS1Barcode/StringExtension.swift
@@ -8,25 +8,32 @@
 
 import Foundation
 
-extension String{
+extension String {
     /** Returning a substring of the current element */
-    func substring(_ from: Int, length: Int)->String{
+    func substring(_ from: Int, length: Int) -> String {
+        guard from >= 0, length >= 0, from < self.count else { return "" }
+        let toIndex = from + length <= self.count ? from + length : self.count
         let start = self.index(self.startIndex, offsetBy: from)
-        let end = self.index(start, offsetBy: length)
+        let end = self.index(self.startIndex, offsetBy: toIndex)
         let range = start..<end
         return String(self[range])
     }
+    
     /** Returning a substring of the current element */
-    func substring(_ from: Int, to: Int)->String{
-        return self.substring(from, length: to-from)
+    func substring(_ from: Int, to: Int) -> String {
+        guard from >= 0, to >= 0, from <= to, to <= self.count else { return "" }
+        return self.substring(from, length: to - from)
     }
+    
     /** Returning a substring of the current element, cutting at the passed char */
-    func substring(from: Int)->String{
+    func substring(from: Int) -> String {
+        guard from >= 0, from <= self.count else { return "" }
         return self.substring(from, length: self.count - from)
     }
     
     /** Returning a substring of the current element, cutting at the passed char */
-    func substring(to: Int)->String{
+    func substring(to: Int) -> String {
+        guard to >= 0, to <= self.count else { return "" }
         return self.substring(0, length: to)
     }
 
@@ -36,7 +43,7 @@ extension String{
     }
     
     /** checks if the string contains a certain substring */
-    func startsWith(_ subString: String) -> Bool{
+    func startsWith(_ subString: String) -> Bool {
         return self.hasPrefix(subString)
     }
 }
